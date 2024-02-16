81ae44fa6cd93d432da069a25705cfad3d6d3aaa
Vishwajit123 <159937882+Vishwajit123@users.noreply.github.com>
2024-02-16 12:13:20 +0530

    Add files via upload

---
+++ arraydeme.java
@@ -0,0 +1,32 @@
+package addition;
+
+public class arraydeme {
+
+	public static void main(String[] args) {
+	int[] a= {8,15,13};
+     int i;
+	int l=0,sl=0;
+	for(i=0;i<a.length;i++){
+		if(a[i]>l){
+			sl=l;
+			l=a[i];
+		} 
+		else if(a[i]>sl && a[i]!=l)
+			sl=a[i];
+	}
+	System.out.println(sl);
+	}
+}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
